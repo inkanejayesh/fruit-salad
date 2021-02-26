c9bbfb9a28a271abb818380e86e3328973e35257
Prateek Celly <prateekcelly@gmail.com>
2021-02-26 15:54:56 +0530

    upadted home-screen

--- src/components/home-screen.js
+++ src/components/home-screen.js
@@ -15,22 +15,21 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import formImage from "../static/images/form-image.jpg";
-import predictionImage from "../static/images/prediction.jpg";
-import {UserInfoForm} from '../user-info-form';
+import { UserInfoForm } from "../user-info-form";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   itemRoot: {
-    display: "flex",
+    // display: "flex",
     padding: theme.spacing(1),
     textAlign: "center",
     color: theme.palette.text.secondary,
-    height: "35vh",
+    height: "70vh",
   },
   grid: {
-    marginTop: "1em",
+    marginTop: "5vh",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -39,28 +38,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const FirstGridItem = () => {
+function handleForecastingClick(e) {
+  window.location.href =
+    "https://foo-bar-dot-hack-fruitsalad.uc.r.appspot.com/";
+}
+
+const FirstGridItem = ({ setLogin }) => {
   const classes = useStyles();
 
   return (
-    <Card elevation={3}>
+    <Card elevation={3} onClick={() => setLogin(true)}>
       <CardActionArea className={classes.itemRoot}>
-        <CardMedia
-          className={classes.image}
-          component="img"
-          alt="Form"
-          height="280"
-          src={formImage}
-          title="Form"
-        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Investment Calculator
           </Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"></Typography>
+          <Typography variant="body1" color="textSecondary" component="p">
+            Whether you're considering getting started with investing or you're
+            already a seasoned investor, an investment calculator can help you
+            figure out how to meet your goals. It can show you how your initial
+            investment, frequency of contributions and risk tolerance can all
+            affect how your money grows.
+          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
@@ -71,32 +70,26 @@ const SecondGridItem = () => {
   const classes = useStyles();
 
   return (
-    <Card elevation={3}>
+    <Card elevation={3} onClick={handleForecastingClick}>
       <CardActionArea className={classes.itemRoot}>
-        <CardMedia
-          className={classes.image}
-          component="img"
-          alt="Form"
-          height="280"
-          src={formImage}
-          title="Form"
-          flex="1"
-        />
         <CardContent flex="1">
           <Typography gutterBottom variant="h5" component="h2">
-            Portfolio Performance
+            Advanced Portfolio Forecasting
           </Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"></Typography>
+          <Typography variant="body1" color="textSecondary" component="p">
+            'NAME' provides the capabilities needed for assessing baseline
+            conditions, forecasting need, proposing alternative planning
+            scenarios, and evaluating complex tradeoffs over the short, medium,
+            and long-term portfolio planning periods based on your current and
+            future needs.
+          </Typography>
         </CardContent>
       </CardActionArea>
     </Card>
   );
 };
 
-const FullWidthGrid = () => {
+const FullWidthGrid = ({ setLogin }) => {
   const classes = useStyles();
 
   return (
@@ -104,16 +97,10 @@ const FullWidthGrid = () => {
       <Container maxWidth={false}>
         <Grid container spacing={3} className={classes.grid}>
           <Grid item xs={6}>
-            <FirstGridItem />
+            <FirstGridItem setLogin={setLogin} />
           </Grid>
           <Grid item xs={6}>
             <SecondGridItem />
-          </Grid>
-          <Grid item xs={6}>
-            <FirstGridItem />
-          </Grid>
-          <Grid item xs={6}>
-            <FirstGridItem />
           </Grid>
         </Grid>
       </Container>
@@ -124,16 +111,16 @@ const FullWidthGrid = () => {
 export const HomeScreen = ({ login, setLogin, setAssistant }) => {
   const { transcript, resetTranscript } = useSpeechRecognition();
 
-  useEffect(() => {
-    let sentence = `Would you like to enable voice assistant for this session?`;
-    let utterance = new SpeechSynthesisUtterance(sentence);
-    speechSynthesis.speak(utterance);
-    utterance.onend = () => {
-      console.log("start speaking");
-      SpeechRecognition.startListening();
-    };
-    SpeechRecognition.startListening();
-  }, [login]);
+  //   useEffect(() => {
+  //     let sentence = `Would you like to enable voice assistant for this session?`;
+  //     let utterance = new SpeechSynthesisUtterance(sentence);
+  //     speechSynthesis.speak(utterance);
+  //     utterance.onend = () => {
+  //       console.log("start speaking");
+  //       SpeechRecognition.startListening();
+  //     };
+  //     SpeechRecognition.startListening();
+  //   }, [login]);
 
   useEffect(() => {
     if (transcript) {
@@ -142,16 +129,13 @@ export const HomeScreen = ({ login, setLogin, setAssistant }) => {
     }
   }, [transcript]);
 
-  return (
-    
-      login ? <UserInfoForm/>
-      :
-      <div>
-        <LoginButton setLogin={setLogin} />
-        {transcript}
-        <FullWidthGrid />
-      </div>
-    
-    
+  return login ? (
+    <UserInfoForm />
+  ) : (
+    <div>
+      <LoginButton setLogin={setLogin} />
+      {transcript}
+      <FullWidthGrid setLogin={setLogin} />
+    </div>
   );
 };
--- src/user-info-form.js
+++ src/user-info-form.js
@@ -1,91 +1,107 @@
-import {Container, Box, TextField, Button, Paper, Grid} from '@material-ui/core'
-import { UserInfo } from './types/user-info';
-import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
-import {useState, useEffect} from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+  Container,
+  Box,
+  TextField,
+  Button,
+  Paper,
+  Grid,
+} from "@material-ui/core";
+import { UserInfo } from "./types/user-info";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useState, useEffect } from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-
-const useStyles = makeStyles(theme => ({
-     paper:{
-         width : '750px',
-         height : '600px',
-         overflow : 'auto',
-         borderRadius : '4px',
-         padding : '32px',
-         '&::-webkit-scrollbar':{
-             width: '8px'
-         },
-         '&::-webkit-scrollbar-track':{
-            background: 'lightgray'
-        },
-        '&::-webkit-scrollbar-thumb':{
-            background: 'gray',
-            borderRadius: '40px'
-        }
-     }
-}))
-
-
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: "750px",
+    height: "600px",
+    overflow: "auto",
+    borderRadius: "4px",
+    padding: "32px",
+    "&::-webkit-scrollbar": {
+      width: "8px",
+    },
+    "&::-webkit-scrollbar-track": {
+      background: "lightgray",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      background: "gray",
+      borderRadius: "40px",
+    },
+  },
+}));
 
 export const UserInfoForm = () => {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    const fieldNames = [
-        "name","gender","age","city","marital status",
-        "highest eduction","usual monthly consumption expenditure",
-        "usual monthly medical expenditure","disability type",
-        "extent of personal assistant required in daliy activity",
-        "arrangement of regular care giver",
-        "infrequent medical expenses in last 365 days",
-        "infrequent non medical expenses in last 365 days",
-        "availing social security benefits",
-        "current monthly income",
-    ];
+  const fieldNames = [
+    "name",
+    "gender",
+    "age",
+    "city",
+    "marital status",
+    "highest eduction",
+    "usual monthly consumption expenditure",
+    "usual monthly medical expenditure",
+    "disability type",
+    "extent of personal assistant required in daliy activity",
+    "arrangement of regular care giver",
+    "infrequent medical expenses in last 365 days",
+    "infrequent non medical expenses in last 365 days",
+    "availing social security benefits",
+    "current monthly income",
+  ];
 
-    const commands = [
-        {
-          command: 'My name is *',
-          //callback: () => video.play()
-        },
-    ];
+  const commands = [
+    {
+      command: "My name is *",
+      //callback: () => video.play()
+    },
+  ];
 
-    useEffect(() => {
-        SpeechRecognition.startListening({continuous:true});
-        // let syn = window.speechSynthesis;
-        //  let utterance = new SpeechSynthesisUtterance("Hello world!");
-        //  syn.speak(utterance);
-    });
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true });
+    // let syn = window.speechSynthesis;
+    //  let utterance = new SpeechSynthesisUtterance("Hello world!");
+    //  syn.speak(utterance);
+  });
 
-    //SpeechRecognition.startListening({continuous:true});
+  //SpeechRecognition.startListening({continuous:true});
 
-    const { transcript, resetTranscript, listening, finalTranscript } = useSpeechRecognition();
-    
-    const [fieldIdx, setFieldIdx] = useState(0);
+  const {
+    transcript,
+    resetTranscript,
+    listening,
+    finalTranscript,
+  } = useSpeechRecognition();
 
-    const itemsRef = React.useRef([]);
+  const [fieldIdx, setFieldIdx] = useState(0);
 
-    useEffect(() => {
-        itemsRef.current = itemsRef.current.slice(0, fieldNames.length);
-     }, [fieldNames]);
+  const itemsRef = React.useRef([]);
 
+  useEffect(() => {
+    itemsRef.current = itemsRef.current.slice(0, fieldNames.length);
+  }, [fieldNames]);
 
-    useEffect(() => {
-       //console.log(`transcript: ${transcript}`);
-       if(finalTranscript){
-       console.log(`${fieldNames[fieldIdx-1]}: ${finalTranscript}`);
-       resetTranscript();
-       setFieldIdx(fieldIdx + 1);
-       let syn = window.speechSynthesis;
-         let utterance = new SpeechSynthesisUtterance(`Please tell us your ${fieldNames[fieldIdx]}.`);
-         syn.speak(utterance);
-       }
+  useEffect(() => {
+    //console.log(`transcript: ${transcript}`);
+    if (finalTranscript) {
+      console.log(`${fieldNames[fieldIdx - 1]}: ${finalTranscript}`);
+      resetTranscript();
+      setFieldIdx(fieldIdx + 1);
+      let syn = window.speechSynthesis;
+      let utterance = new SpeechSynthesisUtterance(
+        `Please tell us your ${fieldNames[fieldIdx]}.`
+      );
+      syn.speak(utterance);
+    }
+  }, [finalTranscript]);
 
-    },[finalTranscript]);
-
-    // useEffect(() => {
+  // useEffect(() => {
   //     if(!listening){
   //         console.log(transcript)
   //         SpeechRecognition.stopListening();
@@ -97,97 +113,108 @@ export const UserInfoForm = () => {
 
   // }, [listening]);s
 
-        const a = new UserInfo();
-    const userInfo = {
-        age:23,
-        gender: "male",
-    };
-    
-    document.onkeydown = function(e) {
-        console.log(e.code);
-        if(e.code === 'KeyQ'){}
-        
-        if(e.code === 'KeyW'){
+  const a = new UserInfo();
+  const userInfo = {
+    age: 23,
+    gender: "male",
+  };
 
-        }
-        if(e.code === 'KeyE'){
-        
-        }
-    };
-
-      const handleStart = () => {
-        let sentence = `Please tell us your ${fieldNames[fieldIdx]}.`;
-        let utterance = new SpeechSynthesisUtterance(sentence);
-        speechSynthesis.speak(utterance);
-        console.log(`${fieldNames[fieldIdx]}: ${finalTranscript}`);
-        setFieldIdx(fieldIdx + 1);
-        
-      }
-
-    const FormDetails = ({fieldNames}) => {
-        const classes = useStyles();
-        
-        return (
-            <Paper className={classes.paper} elevation={3}>
-                <Button onClick={handleStart}>
-                    Start
-                </Button>
-            <form className={classes.root} >
-               
-                <Box display="flex" flexDirection="column">
-                    <Grid container>
-                {fieldNames.map(function (field) {
-                    return(
-                        <Grid key={field} style={{marginBottom: '22px'}} item md={field.split(" ").length > 2 ? 12 : 4}
-                            xs={12}>
-                        <VoiceTextField
-                                
-                                ref={itemsRef.current[fieldIdx]}
-                                id={field}
-                                label={field}
-                                isSpeaking={field === fieldNames[fieldIdx]}
-                                utteranceText={`Please tell us your ${field}.`}
-                            />
-                        </Grid>
-                            );
-                })}
-                </Grid>
-                <Button type="submit">
-                    Submit
-                </Button>
-                </Box>
-            </form>
-        </Paper>
-        );
+  document.onkeydown = function (e) {
+    console.log(e.code);
+    if (e.code === "KeyQ") {
     }
 
-    return(
+    if (e.code === "KeyW") {
+    }
+    if (e.code === "KeyE") {
+    }
+  };
 
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100vw', height: '100vh' }}>
-            <FormDetails fieldNames={fieldNames}/>
-        </div>
-    
+  function handleSubmit() {
+    console.log("Redirecting...");
+    window.location.href =
+      "https://foo-bar-dot-hack-fruitsalad.uc.r.appspot.com";
+  }
 
+  const handleStart = () => {
+    let sentence = `Please tell us your ${fieldNames[fieldIdx]}.`;
+    let utterance = new SpeechSynthesisUtterance(sentence);
+    speechSynthesis.speak(utterance);
+    console.log(`${fieldNames[fieldIdx]}: ${finalTranscript}`);
+    setFieldIdx(fieldIdx + 1);
+  };
+
+  const FormDetails = ({ fieldNames }) => {
+    const classes = useStyles();
+
+    return (
+      <Paper className={classes.paper} elevation={3}>
+        <Button onClick={handleStart}>Start</Button>
+        <form className={classes.root}>
+          <Box display="flex" flexDirection="column">
+            <Grid container>
+              {fieldNames.map(function (field) {
+                return (
+                  <Grid
+                    key={field}
+                    style={{ marginBottom: "22px" }}
+                    item
+                    md={field.split(" ").length > 2 ? 12 : 4}
+                    xs={12}>
+                    <VoiceTextField
+                      ref={itemsRef.current[fieldIdx]}
+                      id={field}
+                      label={field}
+                      isSpeaking={field === fieldNames[fieldIdx]}
+                      utteranceText={`Please tell us your ${field}.`}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+            <Button type="submit" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Box>
+        </form>
+      </Paper>
     );
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}>
+      <FormDetails fieldNames={fieldNames} />
+    </div>
+  );
 };
 
-const VoiceTextField = React.forwardRef(({id, label, utteranceText, isSpeaking}, ref) => {
-
-    return(
-        <Container>
-       <TextField inputRef={el => ref = el} id={id} label={label} 
-           onFocus={
-           () => {
+const VoiceTextField = React.forwardRef(
+  ({ id, label, utteranceText, isSpeaking }, ref) => {
+    return (
+      <Container>
+        <TextField
+          inputRef={(el) => (ref = el)}
+          id={id}
+          label={label}
+          onFocus={() => {
             let synth = window.speechSynthesis;
             let utterance = new SpeechSynthesisUtterance(utteranceText);
-            utterance.onend = function(){
-                SpeechRecognition.startListening();
+            utterance.onend = function () {
+              SpeechRecognition.startListening();
             };
             synth.speak(utterance);
-            }}
-            variant={'outlined'}
-            fullWidth
-       />
-       </Container>
+          }}
+          variant={"outlined"}
+          fullWidth
+        />
+      </Container>
     );
-});
+  }
+);
